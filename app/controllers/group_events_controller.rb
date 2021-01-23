f810a3c7c4bd3db3a7479ad70bd20780f8f57dc6
Arvind <arvindbhadoriya808@gmail.com>
2021-01-23 17:42:12 +0530

    posting event CRUD added

--- app/controllers/group_events_controller.rb
+++ app/controllers/group_events_controller.rb
@@ -1,16 +1,16 @@
 class GroupEventsController < ApplicationController
-  before_action :set_group_event, only: [:show, :update, :destroy]
+  before_action :set_group_event, only: %i[show update destroy]
 
   # GET /group_events
   def index
-    @group_events = GroupEvent.all
+    @group_events = GroupEvent.available.to_show
 
     render json: @group_events
   end
 
   # GET /group_events/1
   def show
-    render json: @group_event
+    render json: @group_event.to_show
   end
 
   # POST /group_events
@@ -18,15 +18,17 @@ class GroupEventsController < ApplicationController
     @group_event = GroupEvent.new(group_event_params)
 
     if @group_event.save
-      render json: @group_event, status: :created, location: @group_event
+      render json: @group_event, status: :created, location: @group_event, message: 'Event published successfully.'
     else
-      render json: @group_event.errors, status: :unprocessable_entity
+      @group_event.status = :draft
+      @group_event.save(validate: false)
+      render json: @group_event, status: :created, location: @group_event, message: 'Event created as draft.'
     end
   end
 
   # PATCH/PUT /group_events/1
   def update
-    if @group_event.update(group_event_params)
+    if @group_event.update_attributes(group_event_params)
       render json: @group_event
     else
       render json: @group_event.errors, status: :unprocessable_entity
@@ -35,17 +37,19 @@ class GroupEventsController < ApplicationController
 
   # DELETE /group_events/1
   def destroy
-    @group_event.destroy
+    @group_event.discarded!
+    render json: {}, status: :ok
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group_event
-      @group_event = GroupEvent.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def group_event_params
-      params.fetch(:group_event, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group_event
+    @group_event = GroupEvent.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def group_event_params
+    params.fetch(:group_event, {}).permit!
+  end
 end
--- app/models/group_event.rb
+++ app/models/group_event.rb
@@ -1,2 +1,37 @@
 class GroupEvent < ApplicationRecord
+  include ActionView::Helpers::TextHelper
+
+  enum status: %i[draft published discarded]
+
+  before_validation :assign_values, on: %i[create update]
+
+  scope :available, -> { where.not(status: 'discarded') }
+
+  validates_presence_of :name, :description, :location, :start_at, :end_at, :duration, :status
+
+  def to_duration
+    "#{duration} Days"
+  end
+
+  def to_description
+    self.description = simple_format(description)
+  end
+
+  def self.to_show
+    all.map(&:to_show)
+  end
+
+  def to_show
+    attributes[:description] = to_description
+    attributes[:duration] = to_duration
+    attributes
+  end
+
+  private
+
+  def assign_values
+    self.start_at ||= end_at - duration.to_i if end_at
+    self.end_at ||= start_at + duration.to_i if start_at
+    self.duration = (end_at - start_at).to_i if start_at && end_at
+  end
 end
